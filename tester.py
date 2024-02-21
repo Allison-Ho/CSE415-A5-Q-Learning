e7ae7f4b12ef4f91884bc411dbe46ba1dec483f0
marinawooden <mwoode@uw.edu>
2024-02-21 00:16:13 -0800

    done for today

--- tester.py
+++ tester.py
@@ -1,8 +1,15 @@
 import toh_mdp as tm
 import solver_utils
+import solvers
 
-config = tm.TohMdpConfig(0.9, 0.0, 0.2, 3, 1)
+config = tm.TohMdpConfig(gamma=1.0, living_reward=0.0, noise=0.2, n_disks=2, n_goals=1)
 mdp = tm.TohMdp.from_config(config)
+solver = solvers.ValueIterationSolver(mdp)
 
-ans = solver_utils.value_iteration(mdp=mdp, v_table={})
-print(ans[2])
+v_table = solver.v_table
+v_table, _, max_delta = solver_utils.value_iteration(mdp=mdp, v_table=v_table)
+print("1st iteration max_delta was " + str(max_delta))
+
+v_table, _, max_delta = solver_utils.value_iteration(mdp=mdp, v_table=v_table)
+print("2nd iteration max_delta was " + str(max_delta))
+
